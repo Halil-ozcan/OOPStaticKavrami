f78a92ef3a465eb2fa3c4210c94d433f4d12531d
Halil Özcan <halil_ozcann@outlook.com>
2024-12-25 21:22:12 +0300

    C# OOP Sinif Static Kavrami Yapıldı

---
+++ OOPStat/Ogrenci.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPStat
+{
+    public class Ogrenci
+    {
+        public string Isim {  get; set; }
+        public string SoyIsim { get; set; }
+
+        public string EmailAdres{ get; set; }
+
+        public void Test1()
+        {
+            Console.WriteLine("Nesne Örneği Alındıktan Sonra Çalışan Metot");
+
+            Test2();
+
+            // static olamayan bir metot static olan bir metotu Çağırabilir.
+        }
+
+        public static void Test2()
+        {
+            Console.WriteLine("Nesne Örneği Alınmadan Kullanılabilir");
+
+            // static olan bir metot static olamayan bir metotu çağıramaz.
+        }
+    }
+}
--- OOPStat/Program.cs
+++ OOPStat/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using OOPStat;
 
 namespace OOPStaticKavrami
 {
@@ -10,6 +11,21 @@ namespace OOPStaticKavrami
     {
         static void Main(string[] args)
         {
+            // Static : nesne örneği alınmadan ulaşabildiğimiz metot, field, sinif...
+
+
+            // Stack Bölge içerisinde pointer ımız oluştu.
+
+            Ogrenci O1;
+
+            // stack bölgede pointer oluştuğu zaman static olan bir yapıcı metot çalıştı ve yine class içerisinde tanımlı olan static, field, metot, static dediğimiz bölgede oluştu.
+            Ogrenci.Test2();
+
+
+            O1 = new Ogrenci();
+            // Stack Bölgede pointer => kendisini heap bölgede tamamladı... static olmayan tanımları tanımladı...
+            O1.Test1();// nesne örneği alındıktan sonra test1 adındaki metotuma ulaştım.
+
         }
     }
 }

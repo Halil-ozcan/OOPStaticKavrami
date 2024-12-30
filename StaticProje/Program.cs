8dc98d7b4e9f35a837b26d6b06e51056cefb5dd8
Halil Özcan <halil_ozcann@outlook.com>
2024-12-30 20:22:14 +0300

    Static Proje

---
+++ StaticProje/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticProje
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
